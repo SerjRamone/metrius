c6261f946d75f84f30cd28f0d4ca59a73fe318aa
serjramone <sergey.greznov@gmail.com>
2024-04-01 02:37:36 +0400

    file configs

--- internal/config/config.go
+++ internal/config/config.go
@@ -2,7 +2,12 @@
 package config
 
 import (
+	"encoding/json"
+	"errors"
 	"flag"
+	"os"
+	"strconv"
+	"strings"
 
 	"github.com/caarlos0/env"
 	"go.uber.org/zap/zapcore"
@@ -15,6 +20,7 @@ const (
 	agentDefaultHashKey        = ""
 	agentDefaultRateLimit      = 1
 	agentDefaultCryptoKey      = ""
+	agentDefaultConfig         = ""
 
 	agentUsageServerAddress  = "address and port of metrics server"
 	agentUsageReportInterval = "period of time for sending data to server in seconds"
@@ -22,6 +28,7 @@ const (
 	agentUsageHashKey        = "key string for hashing function"
 	agentUsageRateLimit      = "number of synchronous outgoing requests"
 	agentUsageCryptoKey      = "path to the public key file"
+	agentUsageConfig         = "path to config.json file"
 
 	serverDefaultAddress         = "localhost:8080"
 	serverDefaultStoreInterval   = 300
@@ -30,6 +37,7 @@ const (
 	serverDefaultDatabaseDSN     = ""
 	serverDefaultHashKey         = ""
 	serverDefaultCryptoKey       = ""
+	serverDefaultConfig          = ""
 
 	serverUsageAddress         = "address and port to run server"
 	serverUsageStoreInterval   = "period of time for put metrics to file"
@@ -38,23 +46,35 @@ const (
 	serverUsageDatabaseDSN     = "data sourse string in format: \"host=host port=port user=myuser password=xxxx dbname=mydb sslmode=disable\""
 	serverUsageHashKey         = "key string for hashing function"
 	serverUsageCryptoKey       = "path to the private key file"
+	serverUsageConfig          = "path to config.json file"
 )
+
+var errTypeAssertion = errors.New("type assesrtion error")
 
 // Agent contents config for Agent
 type Agent struct {
-	ServerAddress  string `env:"ADDRESS"`
+	ServerAddress  string `env:"ADDRESS" json:"address"`
 	HashKey        string `env:"KEY"`
-	CryptoKey      string `env:"CRYPTO_KEY"`
-	ReportInterval int    `env:"REPORT_INTERVAL"`
-	PollInterval   int    `env:"POLL_INTERVAL"`
+	CryptoKey      string `env:"CRYPTO_KEY" json:"crypto_key"`
+	ReportInterval int    `env:"REPORT_INTERVAL" json:"report_interval"`
+	PollInterval   int    `env:"POLL_INTERVAL" json:"poll_interval"`
 	RateLimit      int    `env:"RATE_LIMIT"`
+	Config         string `env:"CONFIG"`
 }
 
 // NewAgent constructor for agent config
 func NewAgent() (Agent, error) {
 	a := Agent{}
 	a.parseFlags()
-	return a, a.parseEnv()
+	if err := a.parseEnv(); err != nil {
+		return a, err
+	}
+	if a.Config != "" {
+		if err := a.parseFile(); err != nil {
+			return a, err
+		}
+	}
+	return a, nil
 }
 
 // parseFlags parse cli flags
@@ -65,6 +85,8 @@ func (c *Agent) parseFlags() {
 	flag.StringVar(&c.HashKey, "k", agentDefaultHashKey, agentUsageHashKey)
 	flag.IntVar(&c.RateLimit, "l", agentDefaultRateLimit, agentUsageRateLimit)
 	flag.StringVar(&c.CryptoKey, "crypto-key", agentDefaultCryptoKey, agentUsageCryptoKey)
+	flag.StringVar(&c.CryptoKey, "c", agentDefaultConfig, agentUsageConfig)
+	flag.StringVar(&c.CryptoKey, "config", agentDefaultConfig, agentUsageConfig)
 
 	flag.Parse()
 }
@@ -72,6 +94,66 @@ func (c *Agent) parseFlags() {
 // parseEnv parse environtment variables
 func (c *Agent) parseEnv() error {
 	return env.Parse(c)
+}
+
+// parseFile parse config file if path setted
+func (c *Agent) parseFile() error {
+	bytes, err := os.ReadFile(c.Config)
+	if err != nil {
+		return err
+	}
+	var tmp map[string]any
+	err = json.Unmarshal(bytes, &tmp)
+	if err != nil {
+		return err
+	}
+
+	var ok bool
+	for param, val := range tmp {
+		if param == "address" && c.ServerAddress == agentDefaultServerAddress {
+			c.ServerAddress, ok = val.(string)
+			if !ok {
+				return errTypeAssertion
+			}
+		}
+		if param == "report_interval" && c.ReportInterval == agentDefaultReportInterval {
+			var v string
+			v, ok = val.(string)
+			if !ok {
+				return errTypeAssertion
+			}
+			if strings.HasSuffix(v, "s") {
+				v, _ = strings.CutSuffix(v, "s")
+			}
+			if interval, err := strconv.Atoi(v); err == nil {
+				c.ReportInterval = interval
+			} else {
+				return err
+			}
+		}
+		if param == "poll_interval" && c.PollInterval == agentDefaultPollInterval {
+			var v string
+			v, ok = val.(string)
+			if !ok {
+				return errTypeAssertion
+			}
+			if strings.HasSuffix(v, "s") {
+				v, _ = strings.CutSuffix(v, "s")
+			}
+			if interval, err := strconv.Atoi(v); err == nil {
+				c.PollInterval = interval
+			} else {
+				return err
+			}
+		}
+		if param == "crypto_key" && c.CryptoKey == agentDefaultCryptoKey {
+			c.CryptoKey, ok = val.(string)
+			if !ok {
+				return errTypeAssertion
+			}
+		}
+	}
+	return nil
 }
 
 // MarshalLogObject zapcore.ObjectMarshaler implemet for loggin agent config struct
@@ -82,25 +164,35 @@ func (c *Agent) MarshalLogObject(enc zapcore.ObjectEncoder) error {
 	enc.AddString("HashKey", c.HashKey)
 	enc.AddInt("RateLimit", c.RateLimit)
 	enc.AddString("CryptoKey", c.CryptoKey)
+	enc.AddString("Config", c.Config)
 	return nil
 }
 
 // Server contents config for Server
 type Server struct {
-	Address         string `env:"ADDRESS"`
-	FileStoragePath string `env:"FILE_STORAGE_PATH"`
-	DatabaseDSN     string `env:"DATABASE_DSN"`
+	Address         string `env:"ADDRESS" json:"address"`
+	FileStoragePath string `env:"FILE_STORAGE_PATH" json:"store_file"`
+	DatabaseDSN     string `env:"DATABASE_DSN" json:"database_dsn"`
 	HashKey         string `env:"KEY"`
-	CryptoKey       string `env:"CRYPTO_KEY"`
-	StoreInterval   int    `env:"STORE_INTERVAL"`
-	Restore         bool   `env:"RESTORE"`
+	CryptoKey       string `env:"CRYPTO_KEY" json:"crypto_key"`
+	StoreInterval   int    `env:"STORE_INTERVAL" json:"store_interval"`
+	Restore         bool   `env:"RESTORE" json:"restore"`
+	Config          string `env:"CONFIG"`
 }
 
 // NewServer constructor for server config
 func NewServer() (Server, error) {
 	s := Server{}
 	s.parseFlags()
-	return s, s.parseEnv()
+	if err := s.parseEnv(); err != nil {
+		return s, err
+	}
+	if s.Config != "" {
+		if err := s.parseFile(); err != nil {
+			return s, err
+		}
+	}
+	return s, nil
 }
 
 // parseFlags parse cli flags
@@ -112,6 +204,8 @@ func (c *Server) parseFlags() {
 	flag.StringVar(&c.DatabaseDSN, "d", serverDefaultDatabaseDSN, serverUsageDatabaseDSN)
 	flag.StringVar(&c.HashKey, "k", serverDefaultHashKey, serverUsageHashKey)
 	flag.StringVar(&c.CryptoKey, "crypto-key", serverDefaultCryptoKey, serverUsageCryptoKey)
+	flag.StringVar(&c.Config, "c", serverDefaultConfig, serverUsageConfig)
+	flag.StringVar(&c.Config, "config", serverDefaultConfig, serverUsageConfig)
 
 	flag.Parse()
 }
@@ -119,6 +213,69 @@ func (c *Server) parseFlags() {
 // parseEnv parse environtment variables
 func (c *Server) parseEnv() error {
 	return env.Parse(c)
+}
+
+// parseFile parse config file if path setted
+func (c *Server) parseFile() error {
+	bytes, err := os.ReadFile(c.Config)
+	if err != nil {
+		return err
+	}
+	var tmp map[string]any
+	err = json.Unmarshal(bytes, &tmp)
+	if err != nil {
+		return err
+	}
+
+	var ok bool
+	for param, val := range tmp {
+		if param == "address" && c.Address == serverDefaultAddress {
+			c.Address, ok = val.(string)
+			if !ok {
+				return errTypeAssertion
+			}
+		}
+		if param == "restore" && c.Restore { // c.Restore is true by default
+			c.Restore, ok = val.(bool)
+			if !ok {
+				return errTypeAssertion
+			}
+		}
+		if param == "store_interval" && c.StoreInterval == serverDefaultStoreInterval {
+			var v string
+			v, ok = val.(string)
+			if !ok {
+				return errTypeAssertion
+			}
+			if strings.HasSuffix(v, "s") {
+				v, _ = strings.CutSuffix(val.(string), "s")
+			}
+			if interval, err := strconv.Atoi(v); err == nil {
+				c.StoreInterval = interval
+			} else {
+				return err
+			}
+		}
+		if param == "store_file" && c.FileStoragePath == serverDefaultFileStoragePath {
+			c.FileStoragePath, ok = val.(string)
+			if !ok {
+				return errTypeAssertion
+			}
+		}
+		if param == "database_dsn" && c.DatabaseDSN == serverDefaultDatabaseDSN {
+			c.DatabaseDSN, ok = val.(string)
+			if !ok {
+				return errTypeAssertion
+			}
+		}
+		if param == "crypto_key" && c.CryptoKey == serverDefaultCryptoKey {
+			c.CryptoKey, ok = val.(string)
+			if !ok {
+				return errTypeAssertion
+			}
+		}
+	}
+	return nil
 }
 
 // MarshalLogObject zapcore.ObjectMarshaler implemet for loggin server config struct
@@ -130,5 +287,6 @@ func (c *Server) MarshalLogObject(enc zapcore.ObjectEncoder) error {
 	enc.AddString("DatabaseDSN", c.DatabaseDSN)
 	enc.AddString("HashKey", c.HashKey)
 	enc.AddString("CryptoKey", c.CryptoKey)
+	enc.AddString("Config", c.Config)
 	return nil
 }
